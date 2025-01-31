f9c83193ce073ca44692fcd46389d0679d11f7ae
AlvisonHunterArnuero <alvison@gmail.com>
2025-01-31 13:21:17 -0600

    refactor(types): enhanced interfaces to avoid repeated keys among them

--- src/Types.ts
+++ src/Types.ts
@@ -4,9 +4,7 @@ export interface Todo {
   completed: boolean;
 }
 
-export interface SortableItemProps {
-  id: string;
+export interface SortableItemProps extends Pick<Todo, "id"| "completed"> {
   children: React.ReactNode;
   onDelete: (id: string) => void;
-  completed: boolean;
 }
