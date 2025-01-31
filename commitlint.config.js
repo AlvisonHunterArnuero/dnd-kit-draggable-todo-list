08d9b4d443fbb72906e825d13ebdf404f7cddd0e
AlvisonHunterArnuero <alvison@gmail.com>
2025-01-31 13:53:10 -0600

    chore(global): added lintstaged config, commitlint & husky pre-commit files

---
+++ commitlint.config.js
@@ -0,0 +1,24 @@
+const config = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'header-max-length': [2, 'always', 100],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'chore',
+        'style',
+        'refactor',
+        'ci',
+        'test',
+        'revert',
+        'perf',
+        'vercel',
+      ],
+    ],
+  },
+};
+export default config;
